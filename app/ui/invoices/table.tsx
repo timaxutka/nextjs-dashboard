e5f6ebb2d5daaff9cfe05bd4d0b01f7ccb7e8c73
Maxim TImofeev <Elevenfive1@gmail.com>
2024-03-12 19:57:36 +0600

    added creation, editing and deletion of accounts, redirection between pages

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -30,7 +30,7 @@ export default async function InvoicesTable({
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
-                        height={28}
+                        height={28} 
                         alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
