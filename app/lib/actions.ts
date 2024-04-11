fec655ed4f4a46c920db69417e1c100a1ac050dc
Maxim TImofeev <Elevenfive1@gmail.com>
2024-04-11 18:17:29 +0600

    added authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,7 +7,9 @@ import { redirect } from "next/navigation";
 import { StaticImageData } from "next/image";
 import { error } from "console";
 import { renderToStaticMarkup } from "react-dom/server";
-
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { isAwaitKeyword } from "typescript";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -119,4 +121,23 @@ export async function deleteInvoice(id: string) {
     } catch (error) {
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try { 
+    await signIn('credentials', formData);
+   } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+   }
 }
